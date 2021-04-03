3926ae52817970cb117795e03cde39df3cd9dad3
Mayank <Mayank13@ibm.com>
2021-04-03 12:09:06 +0530

    Updates

---
+++ precache-manifest.f5de1b8a33495d1eeff40b30eb7668b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71aca8ca394546149221d41dfcbea95d",
+    "url": "/covidTracker/index.html"
+  },
+  {
+    "revision": "26fc289063cd0aff9939",
+    "url": "/covidTracker/static/css/main.bcb79167.chunk.css"
+  },
+  {
+    "revision": "4d1ba0b5fbe73ab2cc11",
+    "url": "/covidTracker/static/js/2.d5318f74.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covidTracker/static/js/2.d5318f74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26fc289063cd0aff9939",
+    "url": "/covidTracker/static/js/main.c25cd8f1.chunk.js"
+  },
+  {
+    "revision": "2442c06dc7c99d913311",
+    "url": "/covidTracker/static/js/runtime-main.0b3102c5.js"
+  }
+]);
